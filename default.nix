c61f4c5d200ff0b7039348242dcead4335f3abf9
Viktor Mitts <vikezor@disroot.org>
2025-08-22 18:06:14 +0300

    oops

--- default.nix
+++ default.nix
@@ -2,7 +2,7 @@
   rustPlatform
 }:
 
-devShells."x86_64-linux".default = pkgs.rustPlatform.buildRustPackage {
+pkgs.rustPlatform.buildRustPackage {
   name = "captchai";
   src = ./.;
   cargoLock.lockFile = ./Cargo.lock;
